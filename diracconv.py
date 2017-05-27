b9cbb5f714f9bc707f1231589b2321bcdbd9ce77
Sergey Zagoruyko <zagoruyko2@gmail.com>
2017-05-27 16:24:24 +0300

    generalize to 1,2,3d

--- diracconv.py
+++ diracconv.py
@@ -5,8 +5,38 @@ import torch.nn.functional as F
 from torch.nn.init import dirac
 
 
-def dirac_delta(in_channels, out_channels, kh, kw):
-    return dirac(torch.Tensor(out_channels, in_channels, kh, kw))
+def dirac_delta(in_channels, out_channels, k):
+    ni, no = in_channels, out_channels
+    n = min(ni, no)
+    size = (n, n) + k
+    repeats = (max(no // ni, 1), max(ni // no, 1)) + (1,) * len(k)
+    return dirac(torch.Tensor(*size)).repeat(*repeats)
+
+
+class DiracConv1d(nn.Conv1d):
+    """Dirac parametrized convolutional layer.
+
+    Works the same way as `nn.Conv1d`, but has additional weight parametrizatoin:
+        :math:`\alpha\delta + \beta W`,
+    where:
+        :math:`\alpha` and :math:`\beta` are learnable scalars,
+        :math:`\delta` is such a tensor so that `F.conv1d(x, delta) = x`, ie
+            Kroneker delta
+        `W` is weight tensor
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, bias=True):
+        super(DiracConv1d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, bias=bias)
+        self.alpha = nn.Parameter(torch.Tensor([5]))
+        self.beta = nn.Parameter(torch.Tensor([1e-5]))
+        self.register_buffer('delta', dirac_delta(in_channels, out_channels, self.weight.size()[2:]))
+        assert self.delta.size() == self.weight.size()
+
+    def forward(self, input):
+        alpha = self.alpha.expand_as(self.weight)
+        beta = self.beta.expand_as(self.weight)
+        return F.conv1d(input, alpha * Variable(self.delta) + beta * self.weight, self.bias, self.stride,
+                        self.padding, self.dilation)
 
 
 class DiracConv2d(nn.Conv2d):
@@ -25,10 +55,37 @@ class DiracConv2d(nn.Conv2d):
         super(DiracConv2d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, bias=bias)
         self.alpha = nn.Parameter(torch.Tensor([5]))
         self.beta = nn.Parameter(torch.Tensor([1e-5]))
-        self.register_buffer('delta', dirac_delta(in_channels, out_channels, self.weight.size(2), self.weight.size(3)))
+        self.register_buffer('delta', dirac_delta(in_channels, out_channels, self.weight.size()[2:]))
+        assert self.delta.size() == self.weight.size()
 
     def forward(self, input):
         alpha = self.alpha.expand_as(self.weight)
         beta = self.beta.expand_as(self.weight)
         return F.conv2d(input, alpha * Variable(self.delta) + beta * self.weight, self.bias, self.stride,
+                        self.padding, self.dilation)
+
+
+class DiracConv3d(nn.Conv3d):
+    """Dirac parametrized convolutional layer.
+
+    Works the same way as `nn.Conv3d`, but has additional weight parametrizatoin:
+        :math:`\alpha\delta + \beta W`,
+    where:
+        :math:`\alpha` and :math:`\beta` are learnable scalars,
+        :math:`\delta` is such a tensor so that `F.conv2d(x, delta) = x`, ie
+            Kroneker delta
+        `W` is weight tensor
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, bias=True):
+        super(DiracConv3d, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, bias=bias)
+        self.alpha = nn.Parameter(torch.Tensor([5]))
+        self.beta = nn.Parameter(torch.Tensor([1e-5]))
+        self.register_buffer('delta', dirac_delta(in_channels, out_channels, self.weight.size()[2:]))
+        assert self.delta.size() == self.weight.size()
+
+    def forward(self, input):
+        alpha = self.alpha.expand_as(self.weight)
+        beta = self.beta.expand_as(self.weight)
+        return F.conv3d(input, alpha * Variable(self.delta) + beta * self.weight, self.bias, self.stride,
                         self.padding, self.dilation)
--- test.py
+++ test.py
@@ -1,20 +1,46 @@
 import unittest
 import torch
-from diracconv import DiracConv2d
+from diracconv import DiracConv1d, DiracConv2d, DiracConv3d
 from torch.autograd import Variable
 
 
 class TestDirac(unittest.TestCase):
 
-    def test_module(self):
+    def test_dirac_property1d(self):
+        ni, no, k, pad = 4, 4, 3, 1
+        module = DiracConv1d(in_channels=ni, out_channels=no, kernel_size=k, padding=pad, bias=False)
+        module.alpha.data.fill_(1)
+        module.beta.data.fill_(0)
+        x = Variable(torch.randn(4, ni, 5))
+        y = module(x)
+        self.assertEqual(y.size(), x.size(), 'shape check')
+        self.assertEqual((y - x).data.abs().sum(), 0, 'dirac delta property check')
+
+    def test_dirac_property2d(self):
         ni, no, k, pad = 4, 4, 3, 1
         module = DiracConv2d(in_channels=ni, out_channels=no, kernel_size=k, padding=pad, bias=False)
         module.alpha.data.fill_(1)
         module.beta.data.fill_(0)
         x = Variable(torch.randn(4, ni, 5, 5))
         y = module(x)
-        self.assertEqual(y.size(), x.size())
-        self.assertEqual((y - x).data.abs().sum(), 0)
+        self.assertEqual(y.size(), x.size(), 'shape check')
+        self.assertEqual((y - x).data.abs().sum(), 0, 'dirac delta property check')
+
+    def test_dirac_property3d(self):
+        ni, no, k, pad = 4, 4, 3, 1
+        module = DiracConv3d(in_channels=ni, out_channels=no, kernel_size=k, padding=pad, bias=False)
+        module.alpha.data.fill_(1)
+        module.beta.data.fill_(0)
+        x = Variable(torch.randn(4, ni, 5, 5, 5))
+        y = module(x)
+        self.assertEqual(y.size(), x.size(), 'shape check')
+        self.assertEqual((y - x).data.abs().sum(), 0, 'dirac delta property check')
+
+    def test_nonsquare(self):
+        ni, no, k, pad = 8, 4, 3, 1
+        module = DiracConv2d(in_channels=ni, out_channels=no, kernel_size=k, padding=pad, bias=False)
+        x = Variable(torch.randn(4, ni, 5, 5))
+        y = module(x)
 
 
 if __name__ == '__main__':
