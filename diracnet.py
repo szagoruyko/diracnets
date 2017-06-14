0d0e0332f690bce16aedc5a3de8f632414d40db2
Sergey Zagoruyko <zagoruyko2@gmail.com>
2017-06-14 08:25:05 +0300

    better model definition

--- diracnet.py
+++ diracnet.py
@@ -85,12 +85,15 @@ def batch_norm(x, params, stats, base, mode):
                         training=mode)
 
 
+def size2name(size):
+    return 'eye' + '_'.join(map(str, size))
+
+
 def block(o, params, stats, base, mode, j):
-    name = base + '.conv'
-    w = params[name]
-    alpha = params[name + '.alpha'].expand_as(w)
-    beta = params[name + '.beta'].expand_as(w)
-    delta = Variable(stats['eye' + '_'.join(map(str, w.size()))])
+    w = params[base + '.conv']
+    alpha = params[base + '.alpha'].expand_as(w)
+    beta = params[base + '.beta'].expand_as(w)
+    delta = Variable(stats[size2name(w.size())])
     w = beta * F.normalize(w.view(w.size(0), -1)).view_as(w) + alpha * delta
     o = F.conv2d(ncrelu(o), w, stride=1, padding=1)
     o = batch_norm(o, params, stats, base + '.bn', mode)
@@ -106,12 +109,13 @@ def group(o, params, stats, base, mode, count):
 def define_diracnet(depth, width, dataset):
 
     def gen_group_params(ni, no, count):
-        return {'block%d' % i: {'conv': conv_params(ni if i == 0 else no, no, k=3, gain=0.1), 'bn': bnparams(no)}
-                for i in range(count)}
+        return {'block%d' % i: {'conv': conv_params(ni if i == 0 else no, no, k=3, gain=0.1),
+                                'alpha': cast(torch.Tensor([5])),
+                                'beta': cast(torch.Tensor([1e-3])),
+                                'bn': bnparams(no)} for i in range(count)}
 
     def gen_group_stats(no, count):
-        return {'block%d' % i: {'bn': bnstats(no)}
-                for i in range(count)}
+        return {'block%d' % i: {'bn': bnstats(no)} for i in range(count)}
 
     if dataset.startswith('CIFAR'):
         n = (depth - 4) // 6
@@ -130,7 +134,7 @@ def define_diracnet(depth, width, dataset):
             return o
 
         params = {
-            'conv': cast(torch.nn.init.kaiming_normal(torch.Tensor(widths[0], 3, 3, 3))),
+            'conv': cast(kaiming_normal(torch.Tensor(widths[0], 3, 3, 3))),
             'bn': bnparams(widths[0]),
             'group0': gen_group_params(widths[0], widths[0], n * 2),
             'group1': gen_group_params(widths[0], widths[1], n * 2),
@@ -163,7 +167,7 @@ def define_diracnet(depth, width, dataset):
             return o
 
         params = {
-            'conv': cast(torch.nn.init.kaiming_normal(torch.Tensor(widths[0], 3, 7, 7))),
+            'conv': cast(kaiming_normal(torch.Tensor(widths[0], 3, 7, 7))),
             'group0': gen_group_params(widths[0], widths[0], 2 * blocks[0]),
             'group1': gen_group_params(widths[0], widths[1], 2 * blocks[1]),
             'group2': gen_group_params(widths[1], widths[2], 2 * blocks[2]),
@@ -184,10 +188,7 @@ def define_diracnet(depth, width, dataset):
     for k, v in flat_params.items():
         if k.find('.conv') > -1:
             no, ni, kh, kw = v.size()
-            I = cast(dirac_delta(ni, no, kh))
             # to optimize for memory we keep only one dirac-tensor per size
-            flat_stats['eye' + '_'.join(map(str,I.size()))] = I
-            flat_params[k + '.alpha'] = Variable(cast(torch.ones(1) * 5), requires_grad=True)
-            flat_params[k + '.beta'] = Variable(cast(torch.ones(1) * 1e-3), requires_grad=True)
+            flat_stats[size2name(v.size())] = cast(dirac_delta(ni, no, kh))
 
     return f, flat_params, flat_stats
